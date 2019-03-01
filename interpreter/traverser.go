1aa0a2cbe08be0530bb63e33cd12373c9fbf7a63
Tommy <tstopak@capital.edu>
2019-03-01 17:42:48 -0500

    parsing strings. update commit.

--- interpreter/traverser.go
+++ interpreter/traverser.go
@@ -32,114 +32,9 @@ func (traverser Traverser) Interpret(token reader.Token) (result string) {
 		isQuote = true
 	}
 	if isQuote {
-		result = "( "
-		if len(token.Children) != 0 {
-			for _, child := range token.Children {
-				//addedQuote := false
-				if child.Value == "(" {
-					child.Value = "'("
-					//addedQuote = true
-				} else if child.Value == ",(" {
-					child.Value = "("
-				}
-				tempResult := traverser.Interpret(*child) + " "
-				/*if addedQuote {
-					tempResult = tempResult[1:]
-				}*/
-				result += tempResult
-			}
-			result += ")"
-		} else {
-			result = "'()"
-		}
-		return result
+		return traverser.generateQuote(token)
 	} else if isList {
-		isDefun := false
-		if string(token.Children[0].Value) == "defun" {
-			isDefun = true
-		}
-		if isDefun {
-			name := token.Children[1].Value
-			params := token.Children[2]
-			body := token.Children[3]
-			traverser.dispatch.Defun(name, *params, *body)
-			return name
-		}
-		isDefParameter := false
-		if string(token.Children[0].Value) == "defparameter" {
-			isDefParameter = true
-		}
-		if isDefParameter {
-			name := token.Children[1].Value
-			value := token.Children[2]
-			traverser.dispatch.Defparameter(name, value)
-			return name
-		}
-		isIf := false
-		if string(token.Children[0].Value) == "if" {
-			isIf = true
-		}
-		if isIf {
-			testResult := traverser.Interpret(*token.Children[1])
-			if testResult == "true" {
-				return traverser.Interpret(*token.Children[2])
-			} else {
-				return traverser.Interpret(*token.Children[3])
-			}
-		}
-		isProgn := false
-		if string(token.Children[0].Value) == "progn" {
-			isProgn = true
-		}
-		if isProgn {
-			if len(token.Children) > 1 {
-				for i := 1; i < len(token.Children)-1; i++ {
-					traverser.Interpret(*token.Children[i])
-				}
-				return traverser.Interpret(*token.Children[len(token.Children)-1])
-			} else {
-				return "()"
-			}
-
-		}
-		isCar := false
-		if string(token.Children[0].Value) == "car" {
-			isCar = true
-		}
-		if isCar {
-			return token.Children[1].Children[0].Value
-
-		}
-		isFunc := false
-		//Create a variable to hold a list in the case this is a function call
-		var funcCall []string = nil
-		// Check if this list is a function call
-		if string(token.Children[0].Value) != "" {
-			isFunc = true
-			// In the case it is instantiate the array
-			funcCall = make([]string, 0, len(token.Children))
-		}
-		// For every child
-		for index, child := range token.Children {
-			// interpret this child recursively and save result
-			var value string
-			value = traverser.Interpret(*child)
-			if index == 0 {
-				value = child.Value
-			}
-
-			//If this list is a function call append all evaluated children to an array
-			if isFunc {
-				funcCall = append(funcCall, value)
-			}
-		}
-		//If the list we just evaluated all of the children for was a function
-		if isFunc {
-			//.Print("Call trace: ")
-			//fmt.Println(funcCall)
-			//Pass the constructed array to the dispatcher to handle the function call
-			result = traverser.dispatch.Call(funcCall)
-		}
+		result = traverser.evaluateList(token)
 	} else {
 		//This was not a list and we can just pass back the literal value of the token
 		if token.Value == "(" {
@@ -155,7 +50,10 @@ func (traverser Traverser) Interpret(token reader.Token) (result string) {
 }
 
 func (traverser Traverser) isSymbol(value string) bool {
-	illegalStartChars := []string{"\""}
+	illegalStartChars := []string{"\"", " "}
+	if value == "" {
+		panic("Unexpected space in list")
+	}
 	for _, illegal := range illegalStartChars {
 		if string(value[0]) == illegal {
 			return false
@@ -171,4 +69,76 @@ func (traverser Traverser) isSymbol(value string) bool {
 		return false
 	}
 	return true
+}
+
+func (traverser Traverser) evaluateList(token reader.Token) string {
+	switch keyword := string(token.Children[0].Value); keyword {
+	case "defun":
+		name := token.Children[1].Value
+		params := token.Children[2]
+		body := token.Children[3]
+		traverser.dispatch.Defun(name, *params, *body)
+		return name
+	case "defparameter":
+		name := token.Children[1].Value
+		value := token.Children[2]
+		traverser.dispatch.Defparameter(name, value)
+		return name
+
+	case "if":
+		testResult := traverser.Interpret(*token.Children[1])
+		if testResult == "true" {
+			return traverser.Interpret(*token.Children[2])
+		} else {
+			return traverser.Interpret(*token.Children[3])
+		}
+	case "progn":
+		if len(token.Children) > 1 {
+			for i := 1; i < len(token.Children)-1; i++ {
+				traverser.Interpret(*token.Children[i])
+			}
+			return traverser.Interpret(*token.Children[len(token.Children)-1])
+		} else {
+			return "()"
+		}
+	case "car":
+		return token.Children[1].Children[0].Value
+	default:
+		funcCall := make([]string, 0, len(token.Children))
+		for index, child := range token.Children {
+			// interpret this child recursively and save result
+			var value string
+			value = traverser.Interpret(*child)
+			if index == 0 {
+				value = child.Value
+			}
+			funcCall = append(funcCall, value)
+		}
+		return traverser.dispatch.Call(funcCall)
+	}
+
+}
+
+func (traverser Traverser) generateQuote(token reader.Token) (result string) {
+	result = "( "
+	if len(token.Children) != 0 {
+		for _, child := range token.Children {
+			//addedQuote := false
+			if child.Value == "(" {
+				child.Value = "'("
+				//addedQuote = true
+			} else if child.Value == ",(" {
+				child.Value = "("
+			}
+			tempResult := traverser.Interpret(*child) + " "
+			/*if addedQuote {
+				tempResult = tempResult[1:]
+			}*/
+			result += tempResult
+		}
+		result += ")"
+	} else {
+		result = "'()"
+	}
+	return result
 }
--- reader/reader.go
+++ reader/reader.go
@@ -13,6 +13,7 @@ type TokenTree struct {
 
 type Token struct {
 	Children []*Token
+	Parent   *Token
 	Value    string
 }
 
@@ -30,32 +31,65 @@ func ReadInput() (tree TokenTree) {
 func formatInput(input string) []string {
 	input = strings.Replace(input, "(", "( ", strings.Count(input, "("))
 	input = strings.Replace(input, ")", " ) ", strings.Count(input, ")"))
+	input = doubleSpaceReplace(input)
 	input = strings.Replace(input, "  ", " ", strings.Count(input, "  "))
 	splitInput := strings.Split(input, " ")
 	return splitInput
 }
 
+func doubleSpaceReplace(input string) string {
+	prev := ""
+	result := ""
+	for _, char := range input {
+		if (prev == " " || prev == "@!@REPLACESPACE@!@") && string(char) == " " {
+			prev = "@!@REPLACESPACE@!@"
+			result += "@!@REPLACESPACE@!@"
+		} else {
+			prev = string(char)
+			result += string(char)
+		}
+	}
+	return result
+}
+
 //(+ (+ 2 3) 2 3 (- 2 3))
 func parse(input []string) (*Token, int) {
-	token := Token{[]*Token{},
+	token := Token{[]*Token{}, nil,
 		input[0]}
 	input = input[1:]
 	popped := 0
 	if token.Value == "(" || token.Value == "'(" || token.Value == ",(" {
 		for input[0] != ")" {
-			if input[0] == "(" || input[0] == "'(" || input[0] == ",(" {
+			if input[0] == "(" || input[0] == "'(" || input[0] == ",(" || string(input[0][0]) == "\"" {
 				tVal, additionPop := parse(input)
 				token.Children = append(token.Children, tVal)
 				input = input[additionPop:]
 				popped += additionPop
 			} else {
-				token.Children = append(token.Children, &Token{[]*Token{}, input[0]})
+				token.Children = append(token.Children, &Token{[]*Token{}, &token, input[0]})
 			}
 			input = input[1:]
 			popped++
 		}
 		input = input[1:]
 		popped++
+	} else if string(token.Value[0]) == "\"" {
+		token.Value = strings.TrimSuffix(token.Value, "\n")
+		result := token.Value
+		tokenLast := string(token.Value[len(token.Value)-1])
+		if tokenLast != "\"" {
+			last := string(input[0][len(input[0])-1])
+			for last != "\"" {
+				result += " " + input[0]
+				input = input[1:]
+				popped++
+				last = string(input[0][len(input[0])-1])
+			}
+			result += " " + input[0]
+			input = input[1:]
+			popped++
+			token.Value = result
+		}
 	}
 	return &token, popped
 }
